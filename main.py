cbb721fc6c1a42b1601aa9d8afeacbad43472f52
Umar Baloch <umarbaloch262@gmail.com>
2023-09-07 21:38:47 +0500

    showing distribution of age via plotly histogram

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 medical_charges_url = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv'
 urlretrieve(medical_charges_url, "medical.csv")
 medical_df = pd.read_csv('medical.csv')
@@ -11,6 +12,13 @@ medical_df = pd.read_csv('medical.csv')
 sns.set_style('darkgrid')
 matplotlib.rcParams['font.size'] = 14
 matplotlib.rcParams['figure.figsize'] = (10, 6)
-matplotlib.rcParams['figure.facecolor'] = '#0000000'
+matplotlib.rcParams['figure.facecolor'] = '#00000000'
 
-print(medical_df.describe())
+print(medical_df.age.describe())
+fig = px.histogram(medical_df,
+                   x='age',
+                   marginal='box',
+                   nbins=47,
+                   title='Distribution pf Age')
+fig.update_layout(bargap=0.1)
+fig.show()
