c32d18ceaf58f3d8f0d4fa8e95873bd254a57fe3
Umar Baloch <umarbaloch262@gmail.com>
2023-09-07 21:32:12 +0500

    added styles

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 medical_charges_url = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv'
 urlretrieve(medical_charges_url, "medical.csv")
-
 medical_df = pd.read_csv('medical.csv')
 
+sns.set_style('darkgrid')
+matplotlib.rcParams['font.size'] = 14
+matplotlib.rcParams['figure.figsize'] = (10, 6)
+matplotlib.rcParams['figure.facecolor'] = '#0000000'
 
 print(medical_df.describe())
